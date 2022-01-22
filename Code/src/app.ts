37c124f9869011c40cc223498259346e6f4dd539
kd41 <kd41@hw.ac.uk>
2022-01-22 18:44:10 +0000

    refactor
    refactored some of the task in the constructor to their own functins. Namely the creation of the scene and canvas

--- Code/src/app.ts
+++ Code/src/app.ts
@@ -8,24 +8,24 @@ import {Color4, Engine, MeshBuilder, Scene} from "@babylonjs/core";
  */
 
 class App {
+    private _canvas: HTMLCanvasElement;
+    private _engine: Engine;
+    private _scene: Scene;
+
     constructor() {
+    
         // create a new canvas which will hold the scene
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";    // make the canvas fit the entire screen
-        canvas.style.height = "100%";
-        canvas.id = "mainCanvas";
-        document.body.appendChild(canvas);
+        this._canvas = this.createCanvas();
 
         // initialize babylon scene and engine
-        var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
-        scene.clearColor = new Color4(0, 0, 0, 1);
+        this._engine = new Engine(this._canvas, true);
+        this._scene = this.createScene();
 
         var box = this.createBox();
 
         // run the render loop
-        engine.runRenderLoop( () => {
-            scene.render();
+        this._engine.runRenderLoop( () => {
+            this._scene.render();
         })
     }
 
@@ -37,6 +37,27 @@ class App {
         box.position.x = 0.5;
         box.position.y = 1;
 
+    }
+
+    /**
+     * Creates a canvas html element then appends it to the scene.
+     * @returns new canvas object
+     */
+    private createCanvas() {
+        var canvas = document.createElement("canvas");
+        canvas.style.width = "100%";    // make the canvas fit the entire screen
+        canvas.style.height = "100%";
+        canvas.id = "mainCanvas";
+        document.body.appendChild(canvas);
+
+        return canvas;
+    }
+
+    private createScene() {
+        var scene = new Scene(this._engine);
+        scene.clearColor = new Color4(0, 0, 0, 1);
+
+        return scene;
     }
 }
 
