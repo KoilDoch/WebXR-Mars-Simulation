24defc669e070578a0d21116cba9bd7ddb584485
kd41 <kd41@hw.ac.uk>
2022-01-22 17:58:37 +0000

    initial canvas scene created

---
+++ Code/src/app.ts
@@ -0,0 +1,31 @@
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import "@babylonjs/loaders/glTF";
+import {Color4, Engine, Scene} from "@babylonjs/core";
+
+/**
+ * This file sets up the scene for development
+ */
+
+class App {
+    constructor() {
+        // create a new canvas which will hold the scene
+        var canvas = document.createElement("canvas");
+        canvas.style.width = "100%";    // make the canvas fit the entire screen
+        canvas.style.height = "100%";
+        canvas.id = "mainCanvas";
+        document.body.appendChild(canvas);
+
+        // initialize babylon scene and engine
+        var engine = new Engine(canvas, true);
+        var scene = new Scene(engine);
+        scene.clearColor = new Color4(0, 0, 0, 1);
+
+        // run the render loop
+        engine.runRenderLoop( () => {
+            scene.render();
+        })
+    }
+}
+
+new App();
